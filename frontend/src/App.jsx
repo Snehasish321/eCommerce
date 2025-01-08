18e52a58cc37e7837c209e8c9cf8e0e28a77ce5b
Snehasish321 <snehasishmondal01@gmail.com>
2025-01-08 07:53:58 +0530

    startNavbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from "./pages/Collection"
+import About from "./pages/About"
+import Product from "./pages/Product"
+import Login from "./pages/Login"
+import PlaceOrder from "./pages/PlaceOrder"
+import Orders from "./pages/Orders"
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path= '/about' element={<About/>}/>
+        <Route path= '/product/:productId' element={<Product/>}/>
+        <Route path='/cart' element={<Product/>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+      </Routes>
       
     </div>
   )
