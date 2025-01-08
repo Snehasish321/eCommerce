f3ba678913dffe26968b0d0a952484adf5aa337b
Snehasish321 <snehasishmondal01@gmail.com>
2025-01-08 19:56:17 +0530

    leeee

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,10 +7,21 @@ const Navbar = () => {
     <div className='flex items-center justify-between py-5 font-medium'>
       <img src={assets.logo} classNamealt='w-36'/>
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
-        <NavLink className='flex flex-col items-center gap-1'>
+        <NavLink to='/' className='flex flex-col items-center gap-1'>
             <p>Home</p>
             <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
-
+        </NavLink>
+        <NavLink to='/collection' className='flex flex-col items-center gap-1'>
+            <p>COLLECTION</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+        </NavLink>
+        <NavLink to='/about' className='flex flex-col items-center gap-1'>
+            <p>ABOUT</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+        </NavLink>
+        <NavLink to='/contact' className='flex flex-col items-center gap-1'>
+            <p>CONTACT</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
         </NavLink>
       </ul>
     </div>
