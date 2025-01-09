33105241df519095dde45b04edd1f8e40c01c1de
Snehasish321 <snehasishmondal01@gmail.com>
2025-01-09 10:37:00 +0530

    background

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,24 +6,34 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       <img src={assets.logo} classNamealt='w-36'/>
-      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+      <ul className='hidden sm:flex gap-5 text-sm text-gray-700 hidden'>
         <NavLink to='/' className='flex flex-col items-center gap-1'>
-            <p>Home</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+            <p>HOME</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
         <NavLink to='/collection' className='flex flex-col items-center gap-1'>
             <p>COLLECTION</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
         <NavLink to='/about' className='flex flex-col items-center gap-1'>
             <p>ABOUT</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
         <NavLink to='/contact' className='flex flex-col items-center gap-1'>
             <p>CONTACT</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
       </ul>
+
+      <div className='flex items-center gap-6'>
+        <img src={assets.search_icon} className='w-5 cursor-pointer' alt=''/>
+        <div className='group relative'>
+          <img className='w-5 cursor-pointer' src={assets.profile_icon}  alt="" />
+          <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
