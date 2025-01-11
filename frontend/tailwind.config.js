7e22e93d6a6e99cb149186b39f511f3c190a16fb
Snehasish321 <snehasishmondal01@gmail.com>
2025-01-11 21:33:50 +0530

    add_cart_icon

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-cream': '#EFEEE5', // Adding your custom color
+      },
+    },
   },
   plugins: [],
 }
