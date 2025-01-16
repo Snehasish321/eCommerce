408970385834697b38ca556f21c244b5fa88dd80
Snehasish321 <snehasishmondal01@gmail.com>
2025-01-16 21:57:12 +0530

    TRIAL

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,15 +1,22 @@
-import React , { useContext } from 'react';
+import React , { useContext, useEffect } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
 
 const LatestCollection = () => {
     const { products } = useContext(ShopContext);
+    const { latestProducts , setLatestProducts } = useContext([]);
+
+    useEffect(() => {
+      setLatestProducts(products.slice(0,10));
+    },[])
 
   return (
     <div className='my-10'>
       <div className='text-center py-8 text-3xl'>
         <Title text1={'LATEST'} text2={'COLLECTION'}/>
-        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'></p>
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste enim distinctio odio saepe optio necessitatibus a porro ipsam architecto consectetur est quam natus repudiandae non facilis praesentium consequuntur, aliquid nemo.
+        </p>
       </div>      
     </div>
   )
---
+++ frontend/src/components/ProductItem.jsx
@@ -0,0 +1,18 @@
+import React , { useContext } from 'react'
+import { Link } from 'react-router-dom';
+import { ShopContext } from '../context/ShopContext';
+
+const ProductItem = ({id, image , name , price}) => {
+
+    const { currency } = useContext(ShopContext);
+
+  return (
+    <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
+      <div className='overflow-hidden'>
+        <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
+      </div>
+    </Link>
+  )
+}
+
+export default ProductItem
