ab1417ef4b8d838cb675c73ba9671761696d680c
Snehasish321 <snehasishmondal01@gmail.com>
2025-01-19 10:32:58 +0530

    bestSeller

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       {/* Logo */}
-      <img src={assets.logo} className="w-48" alt="Logo" />
+      <Link to='/'><img src={assets.logo} className="w-48" alt="Logo" /></Link>
 
       {/* Navbar Links */}
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
