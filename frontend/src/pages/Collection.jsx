baead5827cc288e7c59ae46af596fd09002177eb
Snehasish321 <snehasishmondal01@gmail.com>
2025-01-28 18:55:21 +0530

    sortingGrid

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext , useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
+import Title from '../components/Title';
 
 const Collection = () => {
 
@@ -45,6 +46,21 @@ const Collection = () => {
             </p>
           </div>
         </div>
+      </div>
+      { /*Products dikhao goisss woh bhi right side pe */ }
+      
+      
+      <div className='flex-1'>
+        <div className='flex justify-between text-base sm:text-2xl mb-4'>
+          <Title text1={'ALL'} text2={'COLLECTION'} /> 
+          {/* Sort karlo goisss */}
+          <select className='border-3 border-gray-300 text-sm px-2'>
+            <option value="relavent">Sort by: Relavent</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
+          </select>
+        </div>
+        { /*Products dikhao goisss map ka help se*/}
       </div>
     </div>
   )
