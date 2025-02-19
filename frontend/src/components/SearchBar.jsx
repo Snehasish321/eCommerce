ff6cd1647fd3386d7cca2004bd3e783bc9f3372c
Snehasish321 <snehasishmondal01@gmail.com>
2025-02-19 18:16:21 +0530

    searchEfficiently

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import React , { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
 import { useLocation } from 'react-router-dom';
